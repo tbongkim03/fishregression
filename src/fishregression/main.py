ad3fa694dc31033a1cb14a02af9ff7f5fe4af285
tbongkim03 <tbongkim03@gmial.com>
2024-09-09 18:01:28 +0900

    Update main fastapi

--- src/fishregression/main.py
+++ src/fishregression/main.py
@@ -11,14 +11,14 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("knn")
+@app.get("/knn")
 def read_item(w: float, l: float):
     fish_class = knn_api(l, w)
     result_msg = f"🐟 길이 {l}에 무게 {w}인 물고기는 {fish_class}로 예측됩니다!"
     return {"result": result_msg}
 
 
-@app.get("lr")
+@app.get("/lr")
 def read_item(l: float):
     weight = lr_api(l)
     return {"weight": weight}
